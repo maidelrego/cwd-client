ada9fea60e896007c7610e09c5f9e14023f7e377
Maydel Rego <maidelrego@yahoo.com>
2022-12-10 21:15:02 -0600

    initial files

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-app-bar app>
+      <v-tabs v-model="tab" grow center-active background-color="#e1e2e1" hide-slider centered icons-and-text>
+        <v-tab active-class="primary white--text" to="/">
+          Home
+          <v-icon>mdi-home</v-icon>
+        </v-tab>
+
+        <v-tab active-class="primary white--text" to="quotes">
+          Quotes
+          <v-icon>mdi-file-document</v-icon>
+        </v-tab>
+
+        <v-tab active-class="primary white--text" to="galery">
+          Gallery
+          <v-icon>mdi-folder-multiple-image</v-icon>
+        </v-tab>
+
+        <v-tab active-class="primary white--text" to="aboutUs">
+          About Us
+          <v-icon>mdi-account-group</v-icon>
+        </v-tab>
+
+        <v-tab active-class="primary white--text" to="contactUs">
+          Contact Us
+          <v-icon>mdi-account-box</v-icon>
+        </v-tab>
+
+      </v-tabs>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    tab: null,
+    tabItems: ['Home', 'Quotes', 'Gallery', 'About Us', 'Contact Us']
+  }),
+};
+</script>
